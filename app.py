727145c45dc47c68b04debea2d3d69fc9089dda5
lmalet <43175889+lmalet@users.noreply.github.com>
2018-10-30 16:42:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from Bio.SeqUtils import ProtParam as pp
 from Bio.SeqUtils import seq3
 from plotly import tools
 
-app = dash.Dash()
+app = dash.Dash(_name_)
 server = app.server
 
 ##### 1. DATA #####
